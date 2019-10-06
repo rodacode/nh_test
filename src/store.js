5dcca16a4091da4875454c257484228c97d3ccda
Nico <nicolas.hardmeier@yahoo.es>
2019-10-06 17:34:44 +0200

    added vuex-persistedstate

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 Vue.use(VueAxios, axios)
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   state: {
     inhabitants: []
   },
