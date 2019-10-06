80e9d11928ceef8794d81df58fb72bff0fd3f202
Nico <nicolas.hardmeier@yahoo.es>
2019-10-06 16:22:56 +0200

    added vuex store actions, mutations and getters

--- src/store.js
+++ src/store.js
@@ -1,16 +1,37 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.use(Vuex)
+Vue.use(VueAxios, axios)
 
 export default new Vuex.Store({
   state: {
-
+    inhabitants: []
   },
   mutations: {
-
+    SET_INHABITANTS (state, inhabitants) {
+      state.inhabitants = inhabitants
+    }
   },
   actions: {
+    loadInhabitants ({ commit }) {
+      axios
+        .get('https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json')
+        .then(response => response.data.Brastlewark)
+        .then(inhabitants => {
+        commit('SET_INHABITANTS', inhabitants)
+        })
+    }
 
+  },
+  getters : {
+    getInhabitantById: (state) => (id) => {
+      return state.inhabitants.find(inhabitant => inhabitant.id === id)
+    },
+    getInhabitants: (state) => {
+      return state.inhabitants
+    }
   }
 })
