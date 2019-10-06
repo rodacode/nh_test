cfdb105a2c8ab1dcfcf5807285c980842e1508c4
Nicolas Hardmeier <nicolas.hardmeier@yahoo.es>
2019-10-06 15:30:41 +0200

    first setup and inhabitantsList component creation

--- src/components/InhabitantsList.vue
+++ src/components/InhabitantsList.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="hello">
-
+  <div class="container">
+    <h1 class="title">Brastlewark inhabitants list</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'InhabitantsList'
+
 }
 </script>
 
