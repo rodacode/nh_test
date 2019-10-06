093982e130ff13e939a8ae6ddf683eef30da0e29
Nico <nicolas.hardmeier@yahoo.es>
2019-10-06 17:46:18 +0200

    removed vuex-persistedstate

--- src/store.js
+++ src/store.js
@@ -2,13 +2,11 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 Vue.use(VueAxios, axios)
 
 export default new Vuex.Store({
-  plugins: [createPersistedState()],
   state: {
     inhabitants: []
   },
